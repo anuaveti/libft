850e54031472436fbdde0bbacc9bb1303508f421
Anush Avetisyan <anush.avetisyan@synisys.com>
2023-03-22 14:24:47 +0400

    adding ft_atoi_v2

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+#include <limits.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	tmp;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while ((str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)) && str[i])
+		i++;
+	sign = str[i] == '-' ? -1 : 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	tmp = i;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + str[i++] - 48;
+	if ((res > LONG_MAX || (i - tmp) > 20) && sign == -1)
+		return (0);
+	else if ((res > LONG_MAX || (i - tmp) > 20) && sign == 1)
+		return (-1);
+	else
+		return (res * sign);
+}
