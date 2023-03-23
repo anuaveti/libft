ead4d2e696b3af705326d135c6575bc76eed48cf
anuaveti <123929850+anuaveti@users.noreply.github.com>
2023-03-23 19:18:44 +0400

    adding ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,39 @@
+#include "libft.h"
+
+static int	char_count(int n)
+{
+	int				i;
+	unsigned int	num;
+
+	i = n >= 0 ? 0 : 1;
+	num = n >= 0 ? n : -n;
+	while (num >= 1)
+	{
+		num = num / 10;
+		i++;
+	}
+	return (i);
+}
+
+char		*ft_itoa(int n)
+{
+	int				i;
+	char			*str;
+	int				size;
+	unsigned int	num;
+
+	i = 1;
+	size = n != 0 ? char_count(n) : 1;
+	num = n >= 0 ? n : -n;
+	if (!(str = (char *)malloc((size + 1) * sizeof(char))))
+		return (NULL);
+	str[0] = n != 0 ? '-' : '0';
+	while (num >= 1)
+	{
+		str[size - i] = num % 10 + '0';
+		num = num / 10;
+		i++;
+	}
+	str[size] = '\0';
+	return (str);
+}
